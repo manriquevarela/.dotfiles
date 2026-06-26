18c11e3254bca033f163f90b0c0f86b96b67d160
manrique <manrique.varela@gmail.com>
2026-06-26 12:12:06 -0600

    added trace to remind me when a new version is updated on nixpkgs

--- nixos/overlays/citrix.nix
+++ nixos/overlays/citrix.nix
@@ -1,25 +1,54 @@
 final: prev:
 
 {
-  citrix-workspace = prev.citrix-workspace.overrideAttrs (oldAttrs: rec {
-    version = "26.04.0.105";
+  citrix-workspace =
+    let
+      originalVersion = prev.citrix-workspace.version; # current nixpkgs version
+      expectedVersion = "26.04.0.73"; # this was the last version on nixpkgs when I created the overlay
+      overlayVersion = "26.04.0.105"; # this is the version I downloaded (newer)
+    in
 
-    src = final.requireFile {
-      name = "linuxx64-${version}.tar.gz";
+    builtins.trace
+      (
+        if originalVersion != expectedVersion then
+          ''
+            ⚠️ Citrix Workspace overlay warning:
 
-      sha256 = "1kl6b1ldjd9gb6cmvhxf6ggvc3amq1kz0qwjlb1fp6dxx0pivwm8";
+            nixpkgs citrix-workspace version changed:
+              expected: ${expectedVersion}
+              current:  ${originalVersion}
 
-      message = ''
-        The Citrix Workspace tarball is not automatically downloaded.
+            Consider removing overlays/citrix.nix
+          ''
+        else
+          ''
+            ✓ Citrix Workspace overlay active:
+              nixpkgs version: ${originalVersion}
+              using overlay version: ${overlayVersion}
+          ''
+      )
 
-        Please download:
+      (
+        prev.citrix-workspace.overrideAttrs (oldAttrs: rec {
+          version = overlayVersion;
 
-          linuxx64-${version}.tar.gz
+          src = final.requireFile {
+            name = "linuxx64-${version}.tar.gz";
 
-        from Citrix Workspace App for Linux and add it to the Nix store:
+            sha256 = "1kl6b1ldjd9gb6cmvhxf6ggvc3amq1kz0qwjlb1fp6dxx0pivwm8";
 
-          nix-prefetch-url --type sha256 file://$PWD/linuxx64-${version}.tar.gz
-      '';
-    };
-  });
+            message = ''
+              The Citrix Workspace tarball is not automatically downloaded.
+
+              Please download:
+
+                linuxx64-${version}.tar.gz
+
+              from Citrix Workspace App for Linux and add it to the Nix store:
+
+                nix-prefetch-url --type sha256 file://$PWD/linuxx64-${version}.tar.gz
+            '';
+          };
+        })
+      );
 }
